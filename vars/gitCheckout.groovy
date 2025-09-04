d8aa19c645b04cc5c1e22a3899890c371edf19b0
xeeshanakram <jamzeeshanakram@gmail.com>
2025-09-04 11:46:25 +0500

    update the gitcheckout.groovy file

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,9 @@
-def call(Map stageParams) {
-    checkout{[
-        $class: 'GitSCM',
-        branches: [[name: stageParams.branch]],
-        userRemoteConfigs: [[url: stageParams.url]]
-    ]}
+// vars/gitCheckout.groovy
+def call(Map args = [:]) {
+    def repo   = args.get('repo', 'https://github.com/XEESHANAKRAM/Project-9-java-hands-on.git')
+    def branch = args.get('branch', 'main')
+
+    checkout([$class: 'GitSCM',
+              branches: [[name: "*/${branch}"]],
+              userRemoteConfigs: [[url: repo]]])
 }
