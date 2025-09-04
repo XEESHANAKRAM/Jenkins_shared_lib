15eb76c1d335cc5b562a5d36280e639367c68252
ZEESHAN AKRAM <jamzeeshanakram@gmail.com>
2025-09-04 12:47:50 +0500

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,9 +1,7 @@
-// vars/gitCheckout.groovy
 def call(Map args = [:]) {
-    def repo   = args.get('repo', 'https://github.com/XEESHANAKRAM/Project-9-java-hands-on.git')
-    def branch = args.get('branch', 'main')
-
     checkout([$class: 'GitSCM',
-              branches: [[name: "*/${branch}"]],
-              userRemoteConfigs: [[url: repo]]])
+        branches: [[name: args.branch ?: '*/main']],
+        extensions: [[$class: 'WipeWorkspace']],
+        userRemoteConfigs: [[url: args.repo]]
+    ])
 }
